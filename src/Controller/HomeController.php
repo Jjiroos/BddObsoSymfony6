5369fb66fc2f1525f1c39fbaf400f90ed77f48c2
Jïros <cloclo2412@gmail.com>
2022-03-02 13:20:19 +0100

    ajout des routes, ajout de twig, ajout des recettes

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,29 @@
 
 namespace App\Controller;
 
-class HomeController
-{
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage(): Response
+    {
+        return new Response("<html><h1>Salut la team bien ou quoi ?</h1></html>");
+    }
+
+    /**
+     * @Route("/test/{text}")
+     */
+    public function show($text): Response
+    {
+        $answers = ["toto","tata","j'aime les pâtes","et puis voilà"];
+        return $this->render('home/show.html.twig',[
+            'text'=> ucwords(str_replace('-',' ', $text)),
+            'answers'=> $answers,
+        ]);
+    }
 }
